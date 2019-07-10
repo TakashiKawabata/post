3442914c7cbeb774586fd23407be3c00b3c3ed18
TakashiKawabata <batashi527@gmail.com>
2019-07-10 15:10:27 +0900

    投稿フォーム

--- join/index.php
+++ join/index.php
@@ -1,1 +1,15 @@
 <p>次のフォームに必須事項をご記入ください</p>
+<form action="" method="post" enctype="multipart/form-data">
+  <dl>
+    <dt>ニックネーム<span class="required">必須</span></dt>
+    <dd><input type="text" name="name" size="35" maxlength="255" /></dd>
+    <dt>メールアドレス<span class="required">必須</span></dt>
+    <dd><input type="text" name="email" size="35" maxlength="255" /></dd>
+    <dt>パスワード<span class="required">必須</span></dt>
+    <dd><input type="text" name="password" size="10" maxlength="20" /></dd>
+    <dt>写真など</dt>
+    <dd><input type="file" name="image" size="35" /></dd>
+  </dl>
+  <div><input type="submit" value="入力内容を確認する" /></div>
+
+</form>

7ae96096866ce07dd5124db31892f7e5a51663a7
TakashiKawabata <batashi527@gmail.com>
2019-07-13 14:17:21 +0900

    入力内容の確認

--- join/index.php
+++ join/index.php
@@ -1,3 +1,30 @@
+<?php
+session_start(); //
+
+if (!empty($_POST)){ //ここでPOSTが空でないときに以下を確認（フォームは送信されている）
+  if($_POST['name']==''){
+    $error['name']='blank';
+  }
+  if($_POST)['email']==''{
+    $error['email']='blank';
+  }
+  if(strlen($_POST['password'])<4){ //string + length = strlen
+    $error['password']='length';
+  }
+  if($_POST['password']==''){
+    $error['password']='blank';
+  }
+
+  if(empty($error)){ //$error配列が空ならセッションに値を保存
+    $_SESSION['join']=$_POST;
+    header('Location: check.php');
+    exit();
+  }
+}
+?>
+
+
+
 <p>次のフォームに必須事項をご記入ください</p>
 <form action="index.php" method="post" enctype="multipart/form-data">
   <dl>
